d1989cd86bc8bfc7838fc7db58bdfecc887f270f
evanburchard <evan.burchard@gmail.com>
2009-11-09 16:25:03 -0500

    moved dependencies into test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,25 @@
 require 'rubygems'
 require 'test/unit'
 require 'shoulda'
+require 'factory_girl'
+require 'schema'
+require 'activerecord'
+require 'acts-as-taggable-on'
+
 
 require "#{File.dirname(__FILE__)}/../lib/wetk_twitter"
 
 class Test::Unit::TestCase
 end
+
+
+
+
+
+
+
+
+
+
+
+
