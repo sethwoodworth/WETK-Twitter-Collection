2a83f7aa99434fad1b166c2734148a4478d6bf74
evanburchard <evan.burchard@gmail.com>
2009-11-09 16:22:59 -0500

    commented out some basic tests for crawler planner puller and twiterator

--- test/crawler_test.rb
+++ test/crawler_test.rb
@@ -1,13 +1,13 @@
-require "test_helper"
-
-class PullerTest < Test::Unit::TestCase
-  context "A puller" do
-    setup do
-      @p = Puller.new
-    end
-    should "be able to be created" do
-      @p.inspect
-    end    
-  end
-  
-end
+# require "test_helper"
+# 
+# class PullerTest < Test::Unit::TestCase
+#   context "A puller" do
+#     setup do
+#       @p = Puller.new
+#     end
+#     should "be able to be created" do
+#       @p.inspect
+#     end    
+#   end
+#   
+# end
--- test/twiterator_test.rb
+++ test/twiterator_test.rb
@@ -1,13 +1,13 @@
-require "test_helper"
-
-class TwiteratorTest < Test::Unit::TestCase
-  context "A twiterator" do
-    setup do
-      @p = Twiterator.new
-    end
-    should "be able to be created" do
-      @p.inspect
-    end    
-  end
-  
-end
+# require "test_helper"
+# 
+# class TwiteratorTest < Test::Unit::TestCase
+#   context "A twiterator" do
+#     setup do
+#       @p = Twiterator.new
+#     end
+#     should "be able to be created" do
+#       @p.inspect
+#     end    
+#   end
+#   
+# end
