5763b3dbe1a6c2cce8e24a04d67f7736678ed64c
evanburchard <evan.burchard@gmail.com>
2009-11-09 16:24:00 -0500

    added a test that makes use of factories and ensures that saver can save tweets

--- test/saver_test.rb
+++ test/saver_test.rb
@@ -1,13 +1,18 @@
 require "test_helper"
 
 class SaverTest < Test::Unit::TestCase
-  context "A saver" do
+  context "A default saver" do
     setup do
-      @s = Saver.new
+      @saver = Saver.new
     end
-    should "be able to be created" do
-      @s.inspect
-    end    
+    context "when saving a unique tweet" do
+      setup do
+        @tweet = Factory.build(:tweet)
+        @saver.save(@tweet, &TWEET_SAVE)
+      end
+      should "allow the tweet to be saved" do
+          assert_equal @tweet.text, Tweet.find_by_text(@tweet.text).text
+      end
+    end
   end
-  
 end
