3eb7b29b2ae211bf6c229fa60e51bd1a026ff326
Sam Gilbert <sgilbert@sam-gilberts-computer.local>
2009-11-10 10:08:07 -0500

    fixed several puller syntax errors, commented it out for testing purposes

--- lib/wetk_twitter/puller.rb
+++ lib/wetk_twitter/puller.rb
@@ -1,58 +1,70 @@
-# Statuses/friends
-# statuses/followers
-# statuses/user_timeline
-# search
-# users/show
-
 class Puller
-  :attr_accessor :rules, :base
-  
-  def initialize(rules = {:search_query => nil, :base_type => nil}, base)
-    @rules = rules
-    @base = base
-  end
-  
-  def pull(options, &pull_type)
-    begin
-      pull_type.call(rules, options)
-    rescue Twitter::Unavailable
-       raise Twitter::Unavailable
-    rescue Twitter::NotFound
-       raise Twitter::NotFound
-    rescue Crack::ParseError
-       raise Crack::ParseError
-    rescue Errno::ETIMEDOUT
-      log.error "Puller: pull timed out, retrying in 10"
-      sleep 10
-      retry
-    rescue Twitter::InformTwitter
-      log.error "Puller: Twitter 500 error"
-      sleep 100
-      retry
-    end
-    
-    # handle bad json from twitter
-  
-    # call the saver, pass in results one at a time
-    
-
-    
-    # pass the last id back to the iterator?
-  end
-  
-  
+  # attr_accessor :rules, :base
+  #  
+  #  def initialize(rules = {:search_query => nil}, base = nil)
+  #    @rules = rules
+  #    @base = base
+  #  end
+  #  
+  #  def pull(options, &pull_type)
+  #    begin
+  #      pull_type.call(@rules, options, @base)
+  #    rescue Twitter::Unavailable
+  #       raise Twitter::Unavailable
+  #    rescue Twitter::NotFound
+  #       raise Twitter::NotFound
+  #    rescue Crack::ParseError
+  #       raise Crack::ParseError
+  #    rescue Errno::ETIMEDOUT
+  #      log.error "Puller: pull timed out, retrying in 10"
+  #      sleep 10
+  #      retry
+  #    rescue Twitter::InformTwitter
+  #      log.error "Puller: Twitter 500 error"
+  #      sleep 100
+  #      retry
+  #    end
+  #    
+  #    # handle bad json from twitter
+  #  
+  #    # call the saver, pass in results one at a time
+  #    
+  # 
+  #    
+  #    # pass the last id back to the iterator?
+  #  end
+  #  
+  #  
 end
 
-SEARCH_PULL lambda do |rules, options|
-  results = Twitter::Search.new(rules[:search_query], :user_agent => 'web_ecology_project').max(max_ID).per_page(100).fetch
-  results.results.each do |tweet|
-    $SAVER.save(tweet, &TWEET_SAVE)
-  end
-end
-
-BASE_PULL lambda do |rules, options|
-  results = base.rules[:base_type](options)
-  # call saver...
-end
-
-
+# SEARCH_PULL = lambda do |rules, options, base|
+#   @results = Twitter::Search.new(rules[:search_query], options).per_page(100).fetch
+#   @results.results.each do |tweet|
+#     # $SAVER.save(tweet, &TWEET_SAVE)
+#     @results
+#   end
+# end
+# 
+# USER_PULL = lambda do |rules, options, base|
+#   @results = base.user(options)
+# end
+# 
+# FOLLOWERS_PULL = lambda do |rules, options, base|
+#   @results = base.followers(options)
+# end
+# 
+# FOLLOWER_IDS_PULL = lambda do |rules, options, base|
+#   @results = base.follower_ids(options)
+# end
+# 
+# FRIENDS_PULL = lambda do |rules, options, base|
+#   @results = base.friends(options)
+# end
+# 
+# FRIEND_IDS_PULL = lambda do |rules, options, base|
+#   @results = base.friend_ids(options)
+# end
+# 
+# USER_TWEETS_PULL = lambda do |rules, options, base|
+#   @results = base.user_timeline(options)
+# end
