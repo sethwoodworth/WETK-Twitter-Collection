818f4d3faae56bc379e122472649aa183cf0208d
Sam Gilbert <sgilbert@sam-gilberts-computer.local>
2009-11-11 13:36:30 -0500

    created puller interface, basic pulling tests

--- lib/wetk_twitter/puller.rb
+++ lib/wetk_twitter/puller.rb
@@ -1,70 +1,76 @@
 class Puller
-  # attr_accessor :rules, :base
-  #  
-  #  def initialize(rules = {:search_query => nil}, base = nil)
-  #    @rules = rules
-  #    @base = base
-  #  end
-  #  
-  #  def pull(options, &pull_type)
-  #    begin
-  #      pull_type.call(@rules, options, @base)
-  #    rescue Twitter::Unavailable
-  #       raise Twitter::Unavailable
-  #    rescue Twitter::NotFound
-  #       raise Twitter::NotFound
-  #    rescue Crack::ParseError
-  #       raise Crack::ParseError
-  #    rescue Errno::ETIMEDOUT
-  #      log.error "Puller: pull timed out, retrying in 10"
-  #      sleep 10
-  #      retry
-  #    rescue Twitter::InformTwitter
-  #      log.error "Puller: Twitter 500 error"
-  #      sleep 100
-  #      retry
-  #    end
-  #    
-  #    # handle bad json from twitter
-  #  
-  #    # call the saver, pass in results one at a time
-  #    
-  # 
-  #    
-  #    # pass the last id back to the iterator?
-  #  end
-  #  
-  #  
+  attr_accessor :rules, :base
+    
+    def initialize(rules = {:user_id => nil, :search_query => nil}, base = nil)
+      @rules = rules
+      @base = base
+    end
+    
+    def pull(options = {}, &pull_type)
+      begin
+        pull_type.call(@rules, options, @base)
+      rescue Twitter::Unavailable
+         raise Twitter::Unavailable
+      rescue Twitter::NotFound
+         raise Twitter::NotFound
+      rescue Crack::ParseError
+         raise Crack::ParseError
+      rescue Errno::ETIMEDOUT
+        log.error "Puller: pull timed out, retrying in 10"
+        sleep 10
+        retry
+      rescue Twitter::InformTwitter
+        log.error "Puller: Twitter 500 error"
+        sleep 100
+        retry
+      end
+      
+      # handle bad json from twitter
+    
+      # call the saver, pass in results one at a time
+      
+      # pass the last id back to the iterator?
+    end
+    
+    
 end
 
-# SEARCH_PULL = lambda do |rules, options, base|
-#   @results = Twitter::Search.new(rules[:search_query], options).per_page(100).fetch
-#   @results.results.each do |tweet|
-#     # $SAVER.save(tweet, &TWEET_SAVE)
-#     @results
-#   end
-# end
-# 
-# USER_PULL = lambda do |rules, options, base|
-#   @results = base.user(options)
-# end
-# 
-# FOLLOWERS_PULL = lambda do |rules, options, base|
-#   @results = base.followers(options)
-# end
-# 
-# FOLLOWER_IDS_PULL = lambda do |rules, options, base|
-#   @results = base.follower_ids(options)
-# end
-# 
-# FRIENDS_PULL = lambda do |rules, options, base|
-#   @results = base.friends(options)
-# end
-# 
-# FRIEND_IDS_PULL = lambda do |rules, options, base|
-#   @results = base.friend_ids(options)
-# end
-# 
-# USER_TWEETS_PULL = lambda do |rules, options, base|
-#   @results = base.user_timeline(options)
-# end
+SEARCH_PULL = lambda do |rules, options, base|
+  @results = Twitter::Search.new(rules[:search_query], options).per_page(100).fetch
+  # @results.results.each do |tweet|
+  #    # $SAVER.save(tweet, &TWEET_SAVE)
+  # end
+  @results
+  
+  
+end
+
+USER_PULL = lambda do |rules, options, base|
+  @results = base.user(rules[:user_id],options)
+  @results
+end
+
+FOLLOWERS_PULL = lambda do |rules, options, base|
+  @results = base.followers(options)
+  @results
+end
+
+FOLLOWER_IDS_PULL = lambda do |rules, options, base|
+  @results = base.follower_ids(options)
+  @results
+end
+
+FRIENDS_PULL = lambda do |rules, options, base|
+  @results = base.friends(options)
+  @results
+end
+
+FRIEND_IDS_PULL = lambda do |rules, options, base|
+  @results = base.friend_ids(options)
+  @results
+end
+
+USER_TWEETS_PULL = lambda do |rules, options, base|
+  @results = base.user_timeline(options)
+  @results
+end
--- test/puller_test.rb
+++ test/puller_test.rb
@@ -1,13 +1,62 @@
-# require "test_helper"
-# 
-# class PullerTest < Test::Unit::TestCase
-#   # context "A puller" do
-#   #   setup do
-#   #     @p = Puller.new
-#   #   end
-#   #   should "be able to be created" do
-#   #     @p.inspect
-#   #   end    
-#   # end
-#   # 
-# end
+require "test_helper"
+
+class PullerTest < Test::Unit::TestCase
+  context "An authenticated default puller" do
+    setup do
+      @base = Twitter::Base.new(Twitter::HTTPAuth.new('sam1vp', 'twAg60307', :user_agent => 'web_ecology_project'))
+      @p = Puller.new({}, @base)
+      @user_info_keys_sorted = ["created_at", "description", "favourites_count", "followers_count", "following", "friends_count", "geo_enabled", "id", "location", "name", "notifications", "profile_background_color", "profile_background_image_url", "profile_background_tile", "profile_image_url", "profile_link_color", "profile_sidebar_border_color", "profile_sidebar_fill_color", "profile_text_color", "protected", "screen_name", "status", "statuses_count", "time_zone", "url", "utc_offset", "verified"]
+    end
+  
+    should "be able to be created" do
+      @p.inspect
+    end  
+    
+    should "be able to pull followers from twitter for a given user_id" do
+      @results = @p.pull({:user_id => 15019521}, &FOLLOWERS_PULL)
+      assert_equal @results.first.keys.sort, @user_info_keys_sorted
+      
+    end
+    
+     should "be able to pull an array of user ids from twitter for a given user_id" do
+        @results = @p.pull({:user_id => 15019521}, &FOLLOWER_IDS_PULL)
+        assert_equal @results.class, Array
+        assert_equal @results.first.class, Fixnum
+
+      end
+      
+       should "be able to pull valid json of friends from twitter for a given user_id" do
+          @results = @p.pull({:user_id => 15019521}, &FRIENDS_PULL)
+          assert_equal @results.first.keys.sort, @user_info_keys_sorted
+        end
+        
+         should "be able to pull an array of friend IDs from twitter for a given user_id" do
+            @results = @p.pull({:user_id => 15019521}, &FRIEND_IDS_PULL)
+            assert_equal @results.class, Array
+            assert_equal @results.first.class, Fixnum
+          end
+    
+    context "with a :search_query value in its rules attribute" do
+      setup do
+        @p.rules = {:search_query => 'test'}
+      end
+    
+      should "be able to pull valid json from twitter search" do
+        results = @p.pull({}, &SEARCH_PULL)
+        assert_equal results.keys, ["results", "max_id", "since_id", "refresh_url", "next_page", "results_per_page", "page", "completed_in", "query"]
+      end
+    end
+  
+    context "with a :user_id value in its rules attribute" do 
+      setup do 
+        @p.rules = {:user_id => 15019521}
+      end
+    
+      should "be able to pull info from twitter for that user" do
+        results = @p.pull({}, &USER_PULL)
+        assert_equal results.keys.sort, @user_info_keys_sorted
+        assert_equal results.screen_name, 'sam1vp'
+      end
+    end
+  end
+end
