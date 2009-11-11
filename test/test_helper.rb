fbee333986afe7fe9af0d78d50969c80e8dd6c80
Sam Gilbert <sgilbert@sam-gilberts-computer.local>
2009-11-11 13:35:03 -0500

    added fakewebs and changed test_helper to call it

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,15 @@
 require 'rubygems'
 require 'test/unit'
+require 'fakeweb'
 require 'shoulda'
 require 'factory_girl'
 require 'schema'
 require 'activerecord'
 require 'acts-as-taggable-on'
+require 'twitter'
+require 'fakewebs'
+
+
 
 
 require "#{File.dirname(__FILE__)}/../lib/wetk_twitter"
