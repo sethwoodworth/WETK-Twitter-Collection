724cbe0a904ea82fd2d54be794be6d349f26959d
Sam Gilbert <sgilbert@sam-gilberts-computer.local>
2009-11-11 18:55:35 -0500

    added saver calls for some of the pull procs, added saver object to pull test, created a test for user_timeline pulling, modified tests for friend/follower pulling to reflect cursoring

--- lib/wetk_twitter/puller.rb
+++ lib/wetk_twitter/puller.rb
@@ -7,23 +7,23 @@ class Puller
     end
     
     def pull(options = {}, &pull_type)
-      begin
+      # begin
         pull_type.call(@rules, options, @base)
-      rescue Twitter::Unavailable
-         raise Twitter::Unavailable
-      rescue Twitter::NotFound
-         raise Twitter::NotFound
-      rescue Crack::ParseError
-         raise Crack::ParseError
-      rescue Errno::ETIMEDOUT
-        log.error "Puller: pull timed out, retrying in 10"
-        sleep 10
-        retry
-      rescue Twitter::InformTwitter
-        log.error "Puller: Twitter 500 error"
-        sleep 100
-        retry
-      end
+      # rescue Twitter::Unavailable
+      #          raise Twitter::Unavailable
+      #       rescue Twitter::NotFound
+      #          raise Twitter::NotFound
+      #       rescue Crack::ParseError
+      #          raise Crack::ParseError
+      #       rescue Errno::ETIMEDOUT
+      #         log.error "Puller: pull timed out, retrying in 10"
+      #         sleep 10
+      #         retry
+      #       rescue Twitter::InformTwitter
+      #         log.error "Puller: Twitter 500 error"
+      #         sleep 100
+      #         retry
+      # end
       
       # handle bad json from twitter
     
@@ -37,40 +37,55 @@ end
 
 SEARCH_PULL = lambda do |rules, options, base|
   @results = Twitter::Search.new(rules[:search_query], options).per_page(100).fetch
-  # @results.results.each do |tweet|
-  #    # $SAVER.save(tweet, &TWEET_SAVE)
-  # end
+  @results.results.each do |tweet|
+    $SAVER.save(tweet, &TWEET_SAVE)
+  end
   @results
-  
-  
 end
 
 USER_PULL = lambda do |rules, options, base|
   @results = base.user(rules[:user_id],options)
+  $SAVER.save(@results, &TWITTER_ACCOUNT_SAVE)
   @results
 end
 
 FOLLOWERS_PULL = lambda do |rules, options, base|
   @results = base.followers(options)
+  @results.users.each do
+    #send follower info (and user id?) to Saver
+  end
   @results
 end
 
 FOLLOWER_IDS_PULL = lambda do |rules, options, base|
   @results = base.follower_ids(options)
+  @results.each do
+    #send follower id (and user id?) to Saver
+  end
   @results
 end
 
 FRIENDS_PULL = lambda do |rules, options, base|
   @results = base.friends(options)
+  @results.users.each do
+    #send friend info (and user id?) to Saver
+  end
   @results
 end
 
 FRIEND_IDS_PULL = lambda do |rules, options, base|
   @results = base.friend_ids(options)
+  @results.each do
+    #send follower id (and user id?) to Saver
+  end
   @results
 end
 
 USER_TWEETS_PULL = lambda do |rules, options, base|
+  options[:count] = 200
   @results = base.user_timeline(options)
+  @results.each do
+    #send user tweet to saver
+  end
   @results
 end
--- test/puller_test.rb
+++ test/puller_test.rb
@@ -3,37 +3,43 @@ require "test_helper"
 class PullerTest < Test::Unit::TestCase
   context "An authenticated default puller" do
     setup do
+      $SAVER = Saver.new
       @base = Twitter::Base.new(Twitter::HTTPAuth.new('sam1vp', 'twAg60307', :user_agent => 'web_ecology_project'))
       @p = Puller.new({}, @base)
-      @user_info_keys_sorted = ["created_at", "description", "favourites_count", "followers_count", "following", "friends_count", "geo_enabled", "id", "location", "name", "notifications", "profile_background_color", "profile_background_image_url", "profile_background_tile", "profile_image_url", "profile_link_color", "profile_sidebar_border_color", "profile_sidebar_fill_color", "profile_text_color", "protected", "screen_name", "status", "statuses_count", "time_zone", "url", "utc_offset", "verified"]
+      @user_info_keys = ["created_at", "description", "favourites_count", "followers_count", "following", "friends_count", "geo_enabled", "id", "location", "name", "notifications", "profile_background_color", "profile_background_image_url", "profile_background_tile", "profile_image_url", "profile_link_color", "profile_sidebar_border_color", "profile_sidebar_fill_color", "profile_text_color", "protected", "screen_name", "status", "statuses_count", "time_zone", "url", "utc_offset", "verified"]
     end
   
     should "be able to be created" do
       @p.inspect
     end  
     
-    should "be able to pull followers from twitter for a given user_id" do
-      @results = @p.pull({:user_id => 15019521}, &FOLLOWERS_PULL)
-      assert_equal @results.first.keys.sort, @user_info_keys_sorted
+    should "be able to pull followers for a given user_id and cursor" do
+      @results = @p.pull({:user_id => 15019521, :cursor => -1}, &FOLLOWERS_PULL)
+      assert_same_elements @results.users.first.keys, @user_info_keys
       
     end
     
-    should "be able to pull an array of user ids from twitter for a given user_id" do
+    should "be able to pull an array of user ids for a given user_id" do
       @results = @p.pull({:user_id => 15019521}, &FOLLOWER_IDS_PULL)
       assert_equal @results.class, Array
       assert_equal @results.first.class, Fixnum
 
     end
       
-    should "be able to pull valid json of friends from twitter for a given user_id" do
-      @results = @p.pull({:user_id => 15019521}, &FRIENDS_PULL)
-      assert_equal @results.first.keys.sort, @user_info_keys_sorted
+    should "be able to pull friends for a given user_id and cursor" do
+      @results = @p.pull({:user_id => 15019521, :cursor => -1}, &FRIENDS_PULL)
+      assert_same_elements @results.users.first.keys, @user_info_keys
     end
         
-    should "be able to pull an array of friend IDs from twitter for a given user_id" do
+    should "be able to pull an array of friend IDs for a given user_id" do
       @results = @p.pull({:user_id => 15019521}, &FRIEND_IDS_PULL)
       assert_equal @results.class, Array
       assert_equal @results.first.class, Fixnum
+    end
+    
+    should "be able to pull tweets from a user's timeline for a given user_id" do
+      @results = @p.pull({:user_id => 15019521}, &USER_TWEETS_PULL)
+      assert_same_elements @results.first.keys, ["created_at", "truncated", "favorited", "text", "id", "geo", "in_reply_to_user_id", "in_reply_to_screen_name", "source", "user", "in_reply_to_status_id"]
     end
     
     context "with a :search_query value in its rules attribute" do
@@ -41,7 +47,7 @@ class PullerTest < Test::Unit::TestCase
         @p.rules = {:search_query => 'test'}
       end
     
-      should "results from twitter search should be particular fields" do
+      should "be able to pull a hash from twitter_search with particular fields" do
         results = @p.pull({}, &SEARCH_PULL)
         assert_same_elements results.keys, ["results", "max_id", "since_id", "refresh_url", "next_page", "results_per_page", "page", "completed_in", "query"]
       end
@@ -54,7 +60,7 @@ class PullerTest < Test::Unit::TestCase
     
       should "be able to pull info from twitter for that user" do
         results = @p.pull({}, &USER_PULL)
-        assert_equal results.keys.sort, @user_info_keys_sorted
+        assert_same_elements results.keys, @user_info_keys
         assert_equal results.screen_name, 'sam1vp'
       end
     end
