ecdbdb4c7e623aed403ad170a9018a172bf31791
evanburchard <evan.burchard@gmail.com>
2009-11-13 10:53:09 -0500

    allowed tagging on twitter relationships

--- lib/wetk_twitter/table_classes/twitter_relationship.rb
+++ lib/wetk_twitter/table_classes/twitter_relationship.rb
@@ -1,4 +1,6 @@
 class TwitterRelationship < ActiveRecord::Base
+  acts_as_taggable_on :tags, :projects, :relationship
+  
   belongs_to :friend, :class_name => "TwitterAccount"
   belongs_to :follower, :class_name => "TwitterAccount"  
 end
