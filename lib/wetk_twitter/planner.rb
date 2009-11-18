eff1a0c017e28811b7e11d816ac966ca1de7d213
evanburchard <evan.burchard@gmail.com>
2009-11-17 19:02:32 -0500

    All done

--- lib/wetk_twitter/planner.rb
+++ lib/wetk_twitter/planner.rb
@@ -1,4 +1,6 @@
+require 'ruby-debug'
 class Planner
+  attr_accessor :options
   
   def initialize(base, options)
     $SAVER = Saver.new(options['saving_rules'])
@@ -12,7 +14,7 @@ class Planner
   def pull
     @user_list = $CRAWLER.crawl
     if @user_list.empty? 
-      $TWITERATOR.twiterate({}, {:search_query => options[:search_query]}, &SEARCH_ITER)
+      $TWITERATOR.twiterate({}, {:search_query => self.options['query']['search']}, &SEARCH_ITER)
     else
       get_info(@user_list)
     end
@@ -23,12 +25,12 @@ class Planner
   end
 
   def get_info(users)
-    if options[:get_info][:user_tweets]
+    if options['info_to_get']['user_tweets']
       users.each do |user|
         $TWITERATOR.twiterate({}, {:user_id => user}, &USER_TWEETS_ITER)
       end
     end
-    if option[:get_info][:user_info] && $CRAWLER.crawl_type != FOLLOWERS_CRAWL && $CRAWLER.crawl_type != FRIENDS_CRAWL
+    if options['info_to_get']['user_info'] && $CRAWLER.crawl_type != FOLLOWERS_CRAWL && $CRAWLER.crawl_type != FRIENDS_CRAWL
       users.each do |user|
         $PULLER.pull({:user_id => user}, &USER_INFO_PULL)
       end
