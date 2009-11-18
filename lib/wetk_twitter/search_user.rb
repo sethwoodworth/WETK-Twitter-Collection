98835de14dc0554c397bc2f5e840dbc028cc6d21
Sam Gilbert <sgilbert@sam-gilberts-computer.local>
2009-11-18 18:23:02 -0500

    added user_info attribute to search_user.rb

--- lib/wetk_twitter/search_user.rb
+++ lib/wetk_twitter/search_user.rb
@@ -1,15 +1,16 @@
 require 'ruby-debug'
 class SearchUser
-  attr_accessor :by_id, :by_screen_name, :crawled
+  attr_accessor :by_id, :by_screen_name, :crawled, :user_info
   
   def initialize(opts)
     @by_id = sanitize_id(opts[:by_id])
     @by_screen_name = opts[:by_screen_name]
     @crawled = opts[:crawled]
+    @user_info = opts[:user_info]
   end
 
   def crawled?
-    @craweled
+    @crawled
   end
     
   def search
