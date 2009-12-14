7daa889782160a2694b9b7e34b50a63194343892
Sam Gilbert <sgilbert@wrls12-43.wrls.harvard.edu>
2009-12-14 12:27:35 -0500

    generalized the reply_from method so that it can be used for saving reply_froms, mention_froms, and rt_froms, added default rules, extracted the find_or_create_secondary_account method

--- lib/wetk_twitter/reaction_processor.rb
+++ lib/wetk_twitter/reaction_processor.rb
@@ -1,30 +1,40 @@
 class ReactionProcessor 
 attr_accessor :rules, :user
-  def initialize(user)
+  def initialize(user, rules = {:pull_secondary_influence_user_info => false})
     @rules = rules
     @user = user
     fill_in_user_info
   end
 
-  def rt_from(tweet)
-    
-    
-  end
-
-  def reply_from(reply_screen_name, tweet)
-    reply_twitter_account = TwitterAccount.find_by_screen_name(reply_screen_name)
-    unless reply_twitter_account  
-      if rules[:pull_secondary_influence_user_info]
-        reply_twitter_account = $PULLER.pull({:user => SearchUser.new(:by_screen_name => reply_screen_name)})
+  def process_reactions()  
+    if rules[:mention_from] or rules[:reply_from] or rules[:rt_from]
+      if user.by_id
+        tweet_ids = $TWITERATOR.twiterate({}, {:user_id => user.by_id}, &USER_TWEETS_ITER)[:tweets]
       else
-        reply_twitter_account = TwitterAccount.create(:screen_name => reply_screen_name)
+        tweet_ids = $TWITERATOR.twiterate({}, {:screen_name => user.by_screen_name}, &USER_TWEETS_ITER)[:tweets]        
+      end 
+      tweets_from_tweet_ids(tweet_ids).each do |tweet|
+        influentials = parse_tweet_for_influentials(tweet)
+        if rules[:mention_from] 
+          influentials[:mention_screen_names].each do |screen_name|
+            save_from(screen_name, tweet, 'mention')
+          end
+        end
+        if rules[:reply_from] 
+          save_from(influentials[:reply_screen_name], tweet, 'reply')
+        end
+        if rules[:rt_from]
+          influentials[:rt_screen_names].each do |screen_name|
+            save_from(screen_name, tweet, 'retweet')
+          end
+        end
       end
     end
-    $SAVER.save({:initiator => user.db_user_info, :responder => reply_twitter_account, :tweet => tweet, :type => 'reply'}, &REACTION_SAVE)
-  end
+  end  
 
-  def mention_from
 
+  def save_from(secondary_screen_name, tweet, type)
+    $SAVER.save({:initiator => user.db_user_info, :responder => find_or_create_secondary_account(secondary_screen_name), :tweet => tweet, :type => type}, &REACTION_SAVE)
   end
 
   def mention_to
@@ -40,33 +50,7 @@ attr_accessor :rules, :user
     tweets = tweet_ids.map do |tweet_id| Tweet.find(tweet_id) end
   end
 
-  def process_reactions()
-    # retweets = {}
-    # replies = []
-    # mentions = {}
-
-    if rules[:mention_from] or rules[:reply_from] or rules[:rt_from]
-      if user.by_id
-        tweet_ids = $TWITERATOR.twiterate({}, {:user_id => user.by_id}, &USER_TWEETS_ITER)[:tweets]
-      else
-        tweet_ids = $TWITERATOR.twiterate({}, {:screen_name => user.by_screen_name}, &USER_TWEETS_ITER)[:tweets]        
-      end 
-      tweets = tweets_from_tweet_ids(tweet_ids)
-      tweets.each do |tweet|
-        influentials = parse_tweet_for_influentials(tweet)
-        if rules[:mention_from] 
-          mention_from(influentials[:mention_screen_names], user, tweet.id)
-        end
-        if rules[:reply_from] 
-          reply_from(influentials[:reply_screen_name], user, tweet)
-        end
-        if rules[:rt_from]
-          rt_from(influentials[:rt_screen_names], user, tweet.id)
-        end
-      end
-    end
-  end  
-
+ 
 
   def parse_tweet_for_influentials(tweet)  
     mention_regex = /[@]\w+/
@@ -102,5 +86,17 @@ attr_accessor :rules, :user
       user.db_user_info = $PULLER.pull({:user=>user}, &USER_PULL)
     end
   end
+  
+  def find_or_create_secondary_account(screen_name)
+    account = TwitterAccount.find_by_screen_name(screen_name)
+    unless account  
+      if rules[:pull_secondary_influence_user_info]
+        account = $PULLER.pull({:user => SearchUser.new(:by_screen_name => screen_name)})
+      else
+        account = TwitterAccount.create(:screen_name => screen_name)
+      end
+    end
+  end
+  
 end
 
